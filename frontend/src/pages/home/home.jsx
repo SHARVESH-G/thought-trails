1e6642c6ade2037aad580dcc90d0c6c40ca7a08c
SHARVESH-G <sharveshg2405@gmail.com>
2025-07-16 00:32:56 +0530

    Home Page

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/navbar/navbar";
 import CtmShip from "../../components/chip/chip";
+import BlogList from "../../components/blogList/blogList";
+import BlogCard from "../../components/blogCard/blogCard";
+import { blog_data } from "../../assets/icons/assets";
 
 const Home = () => {
+  const [menu, setMenu] = useState("All");
+
   return (
     <div>
       <Navbar />
-
       <CtmShip text="New : AI Integrated" />
 
       <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-center text-black/65 font-semibold">
@@ -19,7 +23,6 @@ const Home = () => {
         starts right here.
       </p>
 
-
       <form
         className="flex justify-between items-center max-w-lg mx-auto 
                    border border-gray-300 bg-white rounded overflow-hidden 
@@ -31,7 +34,6 @@ const Home = () => {
           required
           className="w-full pl-4 py-4 outline-none text-sm"
         />
-
         <button
           type="submit"
           className="bg-primary text-white px-6 py-2 ml-2 rounded 
@@ -40,6 +42,16 @@ const Home = () => {
           Search
         </button>
       </form>
+
+      <BlogList menu={menu} setMenu={setMenu} />
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40">
+        {blog_data
+          .filter((blog) => menu === "All" || blog.category === menu)
+          .map((blog) => (
+            <BlogCard key={blog._id} blog={blog} />
+          ))}
+      </div>
     </div>
   );
 };
